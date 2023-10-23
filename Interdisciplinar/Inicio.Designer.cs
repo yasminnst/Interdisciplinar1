5121f78bd1bce9a1bdc7fcc15cdb2c3007f9a127
RM20232930054 <RM20232930054@LAB2-13>
2023-10-23 19:40:26 -0300

    Adicionar arquivos de projeto.

---
+++ Interdisciplinar/Inicio.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace Interdisciplinar
+{
+    partial class Inicio
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Inicio));
+            this.TelaSplash = new System.Windows.Forms.PictureBox();
+            this.timerSplash = new System.Windows.Forms.Timer(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.TelaSplash)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TelaSplash
+            // 
+            this.TelaSplash.Image = ((System.Drawing.Image)(resources.GetObject("TelaSplash.Image")));
+            this.TelaSplash.Location = new System.Drawing.Point(0, -6);
+            this.TelaSplash.Name = "TelaSplash";
+            this.TelaSplash.Size = new System.Drawing.Size(1000, 485);
+            this.TelaSplash.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.TelaSplash.TabIndex = 0;
+            this.TelaSplash.TabStop = false;
+            this.TelaSplash.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // timerSplash
+            // 
+            this.timerSplash.Enabled = true;
+            this.timerSplash.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(191, 435);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(618, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // Inicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 461);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.TelaSplash);
+            this.Name = "Inicio";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.TelaSplash_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.TelaSplash)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox TelaSplash;
+        private System.Windows.Forms.Timer timerSplash;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Interdisciplinar/Inicio.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Interdisciplinar
+{
+    public partial class Inicio : Form
+    {
+        public Inicio()
+        {
+            InitializeComponent();
+        }
+
+        private void TelaSplash_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Inicio TelaSplash = new Inicio();
+            TelaSplash.Show();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (progressBar1.Value < 100)
+            {
+                progressBar1.Value = progressBar1.Value + 2;
+            }
+            else
+            {
+                timerSplash.Enabled = false;
+
+
+
+                this.Visible = false;
+            }
+        }
+    }
+}
+
